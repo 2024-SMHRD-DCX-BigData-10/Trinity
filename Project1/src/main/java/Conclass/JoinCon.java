8d8af4def38e21ba3db72e95c2dbc7bb864cf5ee
smhrd1 <smhrd1@smhrd>
2024-06-04 11:30:59 +0900

    0604/창민/모델클래스추가

--- Project1/src/main/java/Conclass/JoinCon.java
+++ Project1/src/main/java/Conclass/JoinCon.java
@@ -13,7 +13,12 @@ public class JoinCon implements Command {
 	@Override
 	public String excute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		System.out.println("[join]");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String age = request.getParameter("age");
+		String gender = request.getParameter("gender");
+		String email  = request.getParameter("email");
 		
 		
 		
---
+++ Project1/src/main/java/model/MemberDTO.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+private String id;
+private String pw;
+private String age;
+private String gender;
+private String email;
+
+}
